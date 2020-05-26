aab82ed54d3d3d9b79497f653f02fbe1a3467f2f
Eric Ung <ungericw@tutanota.com>
2020-05-26 18:04:45 -0500

    5/26/2020 Functional requirements met with some ui changes.

--- CalculatorWithMemory/Controllers/HomeController.cs
+++ CalculatorWithMemory/Controllers/HomeController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CalculatorWithMemory.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using CalculatorWithMemory.Models;
-using System.Linq.Expressions;
-using System.Data;
-using System.Diagnostics.Eventing.Reader;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace CalculatorWithMemory.Controllers
 {
@@ -24,95 +19,13 @@ namespace CalculatorWithMemory.Controllers
 
     public IActionResult Index()
     {
-      IndexViewModel indexViewModel = new IndexViewModel();
-      indexViewModel.Expressions = Expressions;
-      return View(indexViewModel);
-    }
-
-    [HttpPost]
-    public IActionResult Index(String display)
-    {
-      IndexViewModel indexViewModel = new IndexViewModel();
-      indexViewModel.Result = EvaluateExpression(display);
-      indexViewModel.Expressions = Expressions;
-      return View(indexViewModel);
+      return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-
-    private static String EvaluateExpression(String display)
-    {
-      if (display == null)
-      {
-        return "";
-      }
-
-      bool leftNegFlag = false;
-      String left = String.Empty;
-      String right = String.Empty;
-      int operatorSymbol = 0;
-
-      for (int i = 0; i < display.Length; i++)
-      {
-        if (display[i] == '-' && i == 0)
-        {
-          leftNegFlag = true;
-        }
-        else if ((display[i] == '+')||((display[i]== '-')&&(i != 0))||(display[i] == '*')||(display[i] == '/'))
-        {
-          operatorSymbol = i;
-          break;
-        }
-        else
-        {
-          left += display[i];
-        }
-      }
-
-
-      for (int i = operatorSymbol + 1; i < display.Length; i++)
-      {
-        right += display[i];
-      }
-
-      Double leftNumeric, rightNumeric;
-      Double.TryParse(left, out leftNumeric);
-      Double.TryParse(right, out rightNumeric);
-
-      Double result = OperateOnLeftRight(leftNumeric, display[operatorSymbol], rightNumeric);
-
-      QueueView(display + " = " + result);
-
-      return result.ToString();
-    }
-
-    private static Double OperateOnLeftRight(Double left, char operatorSymbol, Double right)
-    {
-      Double result = 0;
-
-      switch(operatorSymbol)
-      {
-        case '+': result = left + right; break;
-        case '-': result = left - right; break;
-        case '*': result = left * right; break;
-        case '/': result = left / right; break;
-      }
-
-      return result;
-    }
-
-    private static void QueueView(String expression)
-    {
-      Expressions.Enqueue(expression);
-
-      if (Expressions.Count > 10)
-      {
-        Expressions.Dequeue();
-      }
     }
   }
 }
--- CalculatorWithMemory/wwwroot/js/site.js
+++ CalculatorWithMemory/wwwroot/js/site.js
@@ -2,7 +2,72 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+"use strict";
 
-function onSubmit(expression, display)
-{
+var connection = new signalR.HubConnectionBuilder().withUrl("/expressionHub").build();
+
+var expressionQueue = new Queue();
+
+var memorySize = 10;
+
+//Disable send button until connection is established
+document.getElementById("submit").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+  var msg = message;
+  var evaluated = eval(message);
+  var encodedMsg = msg + " = " +  evaluated;
+  var li = document.createElement("li");
+  li.textContent = encodedMsg;
+  document.getElementById("messagesList").appendChild(li);
+  updateExpressions(li);
+  document.getElementById("display").value = '';
+  document.getElementById("display").placeholder = evaluated;
+});
+
+connection.start().then(function () {
+  document.getElementById("submit").disabled = false;
+}).catch(function (err) {
+  return console.error(err.toString());
+});
+
+document.getElementById("submit").addEventListener("click", function (event) {
+  var user = document.getElementById("display").value;
+  var message = document.getElementById("display").value;
+  connection.invoke("SendMessage", user, message).catch(function (err) {
+    return console.error(err.toString());
+  });
+  event.preventDefault();
+});
+
+function updateExpressions(newElement) {
+  // Keep variables in check.
+  expressionQueue.enqueue(newElement);
+	document.getElementById("messagesList").innerHTML = '';
+	var newQueue = new Queue();
+
+  if (expressionQueue.getLength() > memorySize) {
+    var current = expressionQueue.dequeue();
+  }
+
+  // Variables to update the display.
+	var counter = 0;
+	var elements = [];
+
+	while (expressionQueue.getLength() > 0) {
+		var current = expressionQueue.dequeue();
+		newQueue.enqueue(current);
+
+		elements[counter] = current;
+		counter++;
+	}
+
+  // Update the queue.
+	expressionQueue = newQueue;
+
+  // Print the previous results.
+	for (var i = elements.length - 1; i >= 0; i--)
+	{
+		document.getElementById("messagesList").appendChild(elements[i]);
+	}
 }
