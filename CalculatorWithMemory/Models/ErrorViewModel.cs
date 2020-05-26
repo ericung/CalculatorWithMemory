43d08fbc80a746395189b4441968929c67979400
Eric Ung <ungericw@tutanota.com>
2020-05-26 12:34:57 -0500

    first commit

---
+++ CalculatorWithMemory/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CalculatorWithMemory.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
