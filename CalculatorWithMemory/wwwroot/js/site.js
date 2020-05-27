79f4c55dce6fd405919af03425653b77ec247cf9
Eric Ung <ungericw@tutanota.com>
2020-05-27 13:30:45 -0500

    5/27/2020 clean code up

--- CalculatorWithMemory/wwwroot/js/site.js
+++ CalculatorWithMemory/wwwroot/js/site.js
@@ -14,15 +14,10 @@ var memorySize = 10;
 document.getElementById("submit").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
-  var msg = message;
-  var evaluated = eval(message);
-  var encodedMsg = msg + " = " +  evaluated;
+  var encodedMsg = message;
   var li = document.createElement("li");
   li.textContent = encodedMsg;
-  document.getElementById("messagesList").appendChild(li);
   updateExpressions(li);
-  document.getElementById("display").value = '';
-  document.getElementById("display").placeholder = evaluated;
 });
 
 connection.start().then(function () {
@@ -33,12 +28,27 @@ connection.start().then(function () {
 
 document.getElementById("submit").addEventListener("click", function (event) {
   var user = document.getElementById("display").value;
-  var message = document.getElementById("display").value;
+  var msg = document.getElementById("display").value;
+	var message = validateExpression(msg);
   connection.invoke("SendMessage", user, message).catch(function (err) {
     return console.error(err.toString());
   });
   event.preventDefault();
 });
+
+function validateExpression(expression) {
+  var evaluated = eval(expression);
+  var message = expression + " = " + evaluated;
+
+  if (evaluated == undefined) {
+    return "No Operation Performed";
+  }
+  else {
+		document.getElementById("display").value = '';
+		document.getElementById("display").placeholder = evaluated;
+		return message;
+	}
+}
 
 function updateExpressions(newElement) {
   // Keep variables in check.
@@ -54,6 +64,7 @@ function updateExpressions(newElement) {
 	var counter = 0;
 	var elements = [];
 
+  // Update the messageslist view.
 	while (expressionQueue.getLength() > 0) {
 		var current = expressionQueue.dequeue();
 		newQueue.enqueue(current);
@@ -71,3 +82,69 @@ function updateExpressions(newElement) {
 		document.getElementById("messagesList").appendChild(elements[i]);
 	}
 }
+
+document.getElementById("clear").addEventListener("click", function (event) {
+  document.getElementById("display").value = '';
+  document.getElementById("display").placeholder = '';
+});
+
+document.getElementById("seven").addEventListener("click", function (event) {
+  document.getElementById("display").value += '7';
+});
+
+document.getElementById("eight").addEventListener("click", function (event) {
+  document.getElementById("display").value += '8';
+});
+
+document.getElementById("nine").addEventListener("click", function (event) {
+  document.getElementById("display").value += '9';
+});
+
+document.getElementById("four").addEventListener("click", function (event) {
+  document.getElementById("display").value += '4';
+});
+
+document.getElementById("five").addEventListener("click", function (event) {
+  document.getElementById("display").value += '5';
+});
+
+document.getElementById("six").addEventListener("click", function (event) {
+  document.getElementById("display").value += '6';
+});
+
+document.getElementById("one").addEventListener("click", function (event) {
+  document.getElementById("display").value += '1';
+});
+
+document.getElementById("two").addEventListener("click", function (event) {
+  document.getElementById("display").value += '2';
+});
+
+document.getElementById("three").addEventListener("click", function (event) {
+  document.getElementById("display").value += '3';
+});
+
+document.getElementById("divide").addEventListener("click", function (event) {
+  document.getElementById("display").value += '/';
+});
+
+document.getElementById("mulitiply").addEventListener("click", function (event) {
+  document.getElementById("display").value += '*';
+});
+
+document.getElementById("minus").addEventListener("click", function (event) {
+  document.getElementById("display").value += '-';
+});
+
+document.getElementById("plus").addEventListener("click", function (event) {
+  document.getElementById("display").value += '+';
+});
+
+document.getElementById("zero").addEventListener("click", function (event) {
+  document.getElementById("display").value += '0';
+});
+
+document.getElementById("dot").addEventListener("click", function (event) {
+  document.getElementById("display").value += '.';
+});
+
